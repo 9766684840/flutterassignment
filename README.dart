88215fd8de7f831bf23e124d53d9d96736c2cb9e
akshay tuptewar <81164399+9766684840@users.noreply.github.com>
2022-07-07 21:38:46 +0530

    Update README.dart

--- README.dart
+++ README.dart
@@ -71,28 +71,28 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
 
-     body:SafeArea(
-       child: Row(
-         children: [
-           Container(
-             color: Colors.purple,
-             height: 100,
-             width: 100,
-             
-           ),
-           Container(
-             color: Colors.amber,
-             height: 100,
-             width: 100,
+      body:SafeArea(
+        child: Row(
+          children: [
+            Container(
+              color: Colors.purple,
+              height: 100,
+              width: 100,
 
-           )
-         ],
-       ),
-     ),
+            ),
+            Container(
+              color: Colors.amber,
+              height: 100,
+              width: 100,
+
+            )
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.happ),
+        child: const Icon(Icons.ads_click),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
